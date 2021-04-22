893a77380144b292cee2613eb4f48f82ed0a3c6b
Brian <briantcheung@gmail.com>
2021-04-22 09:07:19 -0700

    Fixed another schedule bug

--- src/components/CourseSchedule.js
+++ src/components/CourseSchedule.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const formatTime = (time, i) => {
+const timeToString = (time, i) => {
     time = time.split(" ")[1].split("-")[i].split("")
     time.splice(time.indexOf(":"), 1)
     time = time.join("")
@@ -10,56 +10,79 @@ const formatTime = (time, i) => {
     return time
 }
 
+const formatTime = (time) => {
+    let timeArr = [time.substr(0, 2), time.substr(2)]
+    if (parseInt(timeArr[0]) > 12) {
+        timeArr[0] = timeArr[0] - 12
+    }
+    else if (parseInt(timeArr[0]) < 10) {
+        timeArr[0] = timeArr[0].substr(1, 1)
+    }
+    return timeArr.join(":")
+}
+
+const getDay = (block) => block.map(course => course.time.split(" ")[0]).sort((a, b) => a < b)
+
 const CourseSchedule = (props) => {
-    let temp = props.courses.filter(course => props.timeSlot === formatTime(course.time, 0))
+    let block = props.courses.filter(course => props.timeSlot[0] === timeToString(course.time, 0))
     return (
-        <React.Fragment> 
-            <h2 className="time-slot" style={{ gridRow: `time-${props.timeSlot}` }}>{props.timeSlot}</h2>
-            <div className="course monday" style={{ gridColumn: "monday", gridRow: `time-${props.timeSlot}` }}>
-                {temp.length > 0 && temp[0].time.split(" ")[0] === "M-W-F" ?
-                    <div className="scheduleInstance">
-                        <h3 className="course-name">{`${temp[0].subject} ${temp[0].number}`}</h3>
-                        <h3 className="course-title">{temp[0].title}</h3>
-                        <h3 className="course-time">{temp[0].time.split(" ")[1]}</h3>
-                        <h3 className="course-teacher">{temp[0].teacher}</h3>
-                    </div> : null}
-            </div>
-            <div className="course tuesday" style={{ gridColumn: "tuesday", gridRow: `time-${props.timeSlot}` }}>
-                {temp.length > 0 && temp[0].time.split(" ")[0] === "T-Th" ?
-                    <div className="scheduleInstance">
-                        <h3 className="course-name">{`${temp[0].subject} ${temp[0].number}`}</h3>
-                        <h3 className="course-title">{temp[0].title}</h3>
-                        <h3 className="course-time">{temp[0].time.split(" ")[1]}</h3>
-                        <h3 className="course-teacher">{temp[0].teacher}</h3>
-                    </div> : null}
-            </div>
-            <div className="course wednesday" style={{ gridColumn: "wednesday", gridRow: `time-${props.timeSlot}` }}>
-                {temp.length > 0 && temp[0].time.split(" ")[0] === "M-W-F" ?
-                    <div className="scheduleInstance">
-                        <h3 className="course-name">{`${temp[0].subject} ${temp[0].number}`}</h3>
-                        <h3 className="course-title">{temp[0].title}</h3>
-                        <h3 className="course-time">{temp[0].time.split(" ")[1]}</h3>
-                        <h3 className="course-teacher">{temp[0].teacher}</h3>
-                    </div> : null}
-            </div>
-            <div className="course thursday" style={{ gridColumn: "thursday", gridRow: `time-${props.timeSlot}` }}>
-                {temp.length > 0 && temp[0].time.split(" ")[0] === "T-Th" ?
-                    <div className="scheduleInstance">
-                        <h3 className="course-name">{`${temp[0].subject} ${temp[0].number}`}</h3>
-                        <h3 className="course-title">{temp[0].title}</h3>
-                        <h3 className="course-time">{temp[0].time.split(" ")[1]}</h3>
-                        <h3 className="course-teacher">{temp[0].teacher}</h3>
-                    </div> : null}
-            </div>
-            <div className="course friday" style={{ gridColumn: "friday", gridRow: `time-${props.timeSlot}` }}>
-                {temp.length > 0 && temp[0].time.split(" ")[0] === "M-W-F" ?
-                    <div className="scheduleInstance">
-                        <h3 className="course-name">{`${temp[0].subject} ${temp[0].number}`}</h3>
-                        <h3 className="course-title">{temp[0].title}</h3>
-                        <h3 className="course-time">{temp[0].time.split(" ")[1]}</h3>
-                        <h3 className="course-teacher">{temp[0].teacher}</h3>
-                    </div> : null}
-            </div>
+        <React.Fragment>
+            <h2 className="time-slot" style={{ gridRow: `time-${props.timeSlot[0]}` }}>{formatTime(props.timeSlot[0])}</h2>
+            {
+                block.length === 0
+                    ? null
+                    : getDay(block).includes("M-W-F") ?
+                        <div className="course-block" style={{ gridColumn: "monday", gridRow: `time-${props.timeSlot[0]} / time-${props.timeSlot[1]}` }}>
+                            <h5 className="course-name">{`${block[block.length - 1].subject} ${block[block.length - 1].number}`}</h5>
+                            <h5 className="course-title">{block[block.length - 1].title}</h5>
+                            <h5 className="course-time">{block[block.length - 1].time.split(" ")[1]}</h5>
+                            <h5 className="course-teacher">Instructor: {block[block.length - 1].teacher}</h5>
+                        </div> : null
+            }
+            {
+                block.length === 0
+                    ? null
+                    : getDay(block).includes("T-Th") ?
+                        <div className="course-block" style={{ gridColumn: "tuesday", gridRow: `time-${props.timeSlot[0]} / time-${timeToString(block[0].time, 1)}` }}>
+                            <h5 className="course-name">{`${block[0].subject} ${block[0].number}`}</h5>
+                            <h5 className="course-title">{block[0].title}</h5>
+                            <h5 className="course-time">{block[0].time.split(" ")[1]}</h5>
+                            <h5 className="course-teacher">Instructor: {block[0].teacher}</h5>
+                        </div> : null
+            }
+            {
+                block.length === 0
+                    ? null
+                    : getDay(block).includes("M-W-F") ?
+                        <div className="course-block" style={{ gridColumn: "wednesday", gridRow: `time-${props.timeSlot[0]} / time-${props.timeSlot[1]}` }}>
+                            <h5 className="course-name">{`${block[block.length - 1].subject} ${block[block.length - 1].number}`}</h5>
+                            <h5 className="course-title">{block[block.length - 1].title}</h5>
+                            <h5 className="course-time">{block[block.length - 1].time.split(" ")[1]}</h5>
+                            <h5 className="course-teacher">Instructor: {block[block.length - 1].teacher}</h5>
+                        </div> : null
+            }
+            {
+                block.length === 0
+                    ? null
+                    : getDay(block).includes("T-Th") ?
+                        <div className="course-block" style={{ gridColumn: "thursday", gridRow: `time-${props.timeSlot[0]} / time-${timeToString(block[0].time, 1)}` }}>
+                            <h5 className="course-name">{`${block[0].subject} ${block[0].number}`}</h5>
+                            <h5 className="course-title">{block[0].title}</h5>
+                            <h5 className="course-time">{block[0].time.split(" ")[1]}</h5>
+                            <h5 className="course-teacher">Instructor: {block[0].teacher}</h5>
+                        </div> : null
+            }
+            {
+                block.length === 0
+                    ? null
+                    : getDay(block).includes("M-W-F") ?
+                        <div className="course-block" style={{ gridColumn: "friday", gridRow: `time-${props.timeSlot[0]} / time-${props.timeSlot[1]}` }}>
+                            <h5 className="course-name">{`${block[block.length - 1].subject} ${block[block.length - 1].number}`}</h5>
+                            <h5 className="course-title">{block[block.length - 1].title}</h5>
+                            <h5 className="course-time">{block[block.length - 1].time.split(" ")[1]}</h5>
+                            <h5 className="course-teacher">Instructor: {block[block.length - 1].teacher}</h5>
+                        </div> : null
+            }
         </React.Fragment>
     )
 }
