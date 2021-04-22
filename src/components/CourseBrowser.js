1246f71da602f848d84a3a5e514e866ef2fe6752
Brian <briantcheung@gmail.com>
2021-04-22 15:36:34 -0700

    bugfixing

--- src/components/CourseBrowser.js
+++ src/components/CourseBrowser.js
@@ -22,15 +22,19 @@ class CourseBrowser extends React.Component {
         return courses
     }
 
-    render() {
+    getSubjects = () => {
         let subjects = []
         for (let c of this.props.courses) {
             !subjects.includes(c.subject) && subjects.push(c.subject)
         }
+        return subjects
+    }
+
+    render() {
         return (
             <div className="mainContainer">
                 {this.props.showing === "all" ? <h1>Browse Courses</h1> : <h1>My Courses</h1>}
-                {this.props.showing === "all" ? <CourseFilter subjects={subjects} onChange={this.setSubjectFilters} /> : null}
+                {this.props.showing === "all" ? <CourseFilter subjects={this.getSubjects()} onChange={this.setSubjectFilters} /> : null}
                 <div className="courseContainer">
                     {this.props.showing === "all" ? 
                     this.filterCourses().map(course => <Course course={course} key={course.id} onClick={this.props.onClick} />) : 
