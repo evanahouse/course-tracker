54b09ef24e4c0b18904f76f1031bf9b8ef3807dd
Brian <briantcheung@gmail.com>
2021-04-21 17:43:44 -0700

    Added course filter

--- src/components/CourseBrowser.js
+++ src/components/CourseBrowser.js
@@ -1,15 +1,44 @@
 import React from "react"
 import Course from "../components/Course"
+import CourseFilter from "./CourseFilter"
 
-const CourseBrowser = (props) => {
-    return (
-        <div className="mainContainer">
-            {props.showing === "all" ? <h1>Browse Courses</h1> : <h1>My Courses</h1>}
-            <div className="courseContainer">
-                {props.courses.map(course => <Course course={course} key={course.id} onClick={props.onClick}/>)}
+class CourseBrowser extends React.Component {
+
+    state = {
+        filters: {
+            subject: "all"
+        }
+    }
+
+    setSubjectFilters = (e) => {
+        this.setState({ filters: { ...this.state.filters, subject: e.target.value } })
+    }
+
+    filterCourses = () => {
+        let courses = this.props.courses
+        if (this.state.filters.subject !== "all") {
+            courses = courses.filter(c => this.state.filters.subject === c.subject);
+        }
+        return courses
+    }
+
+    render() {
+        let subjects = []
+        for (let c of this.props.courses) {
+            !subjects.includes(c.subject) && subjects.push(c.subject)
+        }
+        return (
+            <div className="mainContainer">
+                {this.props.showing === "all" ? <h1>Browse Courses</h1> : <h1>My Courses</h1>}
+                {this.props.showing === "all" ? <CourseFilter subjects={subjects} onChange={this.setSubjectFilters} /> : null}
+                <div className="courseContainer">
+                    {this.props.showing === "all" ? 
+                    this.filterCourses().map(course => <Course course={course} key={course.id} onClick={this.props.onClick} />) : 
+                    this.props.courses.map(course => <Course course={course} key={course.id} onClick={this.props.onClick} />)}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default CourseBrowser
